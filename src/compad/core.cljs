13fb3d9a21461c33e0b5d47c7db99666b37210cb
Toby Crawley <toby@tcrawley.org>
2019-03-11 22:04:17 -0400

    Allow adding creatures

--- src/compad/core.cljs
+++ src/compad/core.cljs
@@ -5,12 +5,10 @@
 
 (defonce app-state (atom {:active-creature 0
                           :round 1
-                          :creatures
-                          (sorted-map 0 {:name "Honk"
-                                         :init 10
-                                         :init-mod 8})}))
+                          :creatures [{:name "Honk"
+                                       :init 10
+                                       :init-mod 8}]}))
 
-;; (swap! app-state assoc-in [:creatures 1] {:name "Iggy" :init 10 :init-mod -1})
 
 (defn get-app-element []
   (gdom/getElement "app"))
@@ -20,9 +18,36 @@
     (str "+" v)
     (str v)))
 
+(defn parse-entry
+  [e]
+  (let [[_ name init-mod] (re-find #"([^|]+)\|?(.*)" e)]
+    [name (if (empty? init-mod) 0 (js/parseInt init-mod))]))
+
+(defn new-creature
+  [name init-mod]
+  {:name name
+   :init 10
+   :init-mod init-mod})
+
+(defn add-creature []
+  (let [val (atom nil)]
+    (fn []
+      [:div.add-creature
+       [:input
+        {:type "text"
+         :placeholder "name|init-mod"
+         :on-change #(reset! val (-> % .-target .-value))
+         :on-key-down (fn [e]
+                        (when
+                            (and (= 13 (.-which e))
+                                 (not (empty? @val)))
+                          (swap! app-state update
+                                 :creatures conj (apply new-creature (parse-entry @val)))
+                          (set! (.-value (.-target e)) "")))}]])))
+
 (defn counter []
   (let [count-state (atom 0)]
-    (fn [parent-state {:keys [value name visible? position close]}]
+    (fn [parent-state {:keys [value visible? position close]}]
       (let [[x y] position
             close' (fn []
                      (reset! count-state 0)
@@ -30,7 +55,7 @@
         [:div {:class ["counter" (when (not visible?) "hidden")]
                :style {:top (str y "px") :left (str x "px")}}
          [:button {:on-click close'} "x"]
-         [:span (str name ": " (+ value @count-state) " (" (with-sign @count-state) ")")]
+         [:span.bold (str (+ value @count-state) " (" (with-sign @count-state) ")")]
          [:button {:on-click #(swap! count-state + 10)} (with-sign 10)]
          [:button {:on-click #(swap! count-state + 5)} (with-sign 5)]
          [:button {:on-click #(swap! count-state + 1)} (with-sign 1)]
@@ -44,17 +69,16 @@
         toggle-counter #(swap! creature-state update % not)]
     (fn [{:keys [name init init-mod] :as creature} idx active?]
       [:div {:class ["creature" (when active? "active")]}
-       [:span.name name]
+       [:span.bold.name name]
        [:span.init-mod (with-sign init-mod)]
        [:span "Init: " init
         [:button {:on-click (fn [e]
                               (let [pos (.getBoundingClientRect (.-target e))]
                                 (toggle-counter :init-visible?)
-                                (swap! creature-state assoc :counter-position [(.-x pos) (.-y pos)])))}
+                                (swap! creature-state assoc :counter-position [(.-x pos) (- (.-y pos) 2)])))}
          "+"]
         [counter (cursor app-state [:creatures idx :init])
          {:value init
-          :name "Init"
           :visible? (:init-visible? @creature-state)
           :position (:counter-position @creature-state)
           :close (partial toggle-counter :init-visible?)}]]])))
@@ -72,12 +96,15 @@
   (let [{:keys [active-creature creatures round]} @app-state]
     [:div
      [:div (str @app-state)]
+     [add-creature]
      [:div.controls
       [:span.round "Round: " round]
       [:button.next {:on-click next-creature} ">>"]]
      [:div
-      (for [[idx c] creatures]
-        ^{:key idx} [creature c idx (= idx active-creature)])]]))
+      (map-indexed
+        (fn [idx c]
+          ^{:key idx} [creature c idx (= idx active-creature)])
+        creatures)]]))
 
 (defn mount [el]
   (reagent/render-component [encounter] el))
